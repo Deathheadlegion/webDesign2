a796e73b8b5640967b85588365da6f294cffbffe
Christian Enriquez <enriquezchris2@gmail.com>
2019-05-07 17:02:50 -0700

    fixed css
    removed some animated hamburger menu I failed to achieve

--- HW/class11_hw/main.js
+++ HW/class11_hw/main.js
@@ -1,13 +1,16 @@
-
-function myFunction(x) {
+/*function myFunction(x) {
   x.classList.toggle("change");
 }
+*/
 
-function myFunction() {
+function myFunction(x) {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
       x.className += " responsive";
+      
     } else {
       x.className = "topnav";
+      
     }
 }
+
