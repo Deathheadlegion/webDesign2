d33d6cb7227bb1e8b64e70a1b3b260488310cf79
Christian Enriquez <enriquezchris2@gmail.com>
2019-06-04 19:05:22 -0700

    tweaked greeting

--- HW/classFinal/main.js
+++ HW/classFinal/main.js
@@ -6,3 +6,15 @@ var $hamburger = $(".hamburger");
         $("#myLinks").slideToggle(400);
 
     });
+
+    // $( ".donate-button" ).click(function( event ) {
+    //     event.preventDefault();
+    //     $( "<div>" )
+    //       .append( "default " + event.type + " prevented" )
+    //       .appendTo( "#log" );
+    //   });
+var $greeting = $(".donate-button");
+      $greeting.on("click", function() {
+          $greeting.toggle(".greeting");
+          $(".greeting").slideToggle(400);
+      });
